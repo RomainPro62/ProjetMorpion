11515ec2f92348c09724b9cdb0a1d5462f18703a
E20210406 <romain62.bdltgmail.com>
2021-12-14 16:51:33 +0100

    Initial commit

---
+++ Script.js
@@ -0,0 +1,9 @@
+function estvalide (table){
+    return table.innerHTML.length;
+}
+function rechercherVainqueur(pions, joueurs, tours) {
+    pions[0].innerHTML === joueurs [tour]
+}
+if (A1 === "X" && A2 === "X" && A3 === "X"){
+    console.log("X a gagné");
+}
